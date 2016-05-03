ce4739c6f0423bd53d5a0b9bf1be11b9a70f4856
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-05-03 11:20:01 -0400

    Update aframe-faceset-component.js

--- components/mesh/aframe-faceset-component.js
+++ components/mesh/aframe-faceset-component.js
@@ -126,10 +126,25 @@ function getGeometry (data) {
   });
   
   //if no triangles triangulate
-  //
-  data.triangles.forEach(function (vec3) {
+  //vertices 2d array of arrays [[2, 4], [5, 6]]
+  //var triangles = Delaunay.triangulate(2dvertices);
+  //triangles flat array of indices [0, 1, 2,   2, 1, 3 ]
+  //find shortest dimension and ignore it for 2d vertices
+  //bb = geometry.computeBoundingBox or so
+  //size = bb.max - bb.min;
+  //xd='x',yd='y' // z smallest
+  // if size.x < size.y && size.x < size.z {xd='y',yd='z'}
+  // if size.y < size.x && size.y < size.z {xd='x',yd='z'}
+  //2dvertices = vertices.map(function(vtx){return [ vtx[xd], vtx[yd] ]});
+  //for (var i=0; i < triangles.length; i+=3) {
+    //geometry.faces.push(
+    // new THREE.Face3(triangles[i],triangles[i+1],triangles[i+2])
+    //);
+  //}
+  
+  data.triangles.forEach(function (fac3) {
     geometry.faces.push(
-      new THREE.Face3(vec3.x, vec3.y, vec3.z)
+      new THREE.Face3(fac3.x, fac3.y, fac3.z)
     );
   });
   
