e56109da3c591db22a6ed314778208e753c4d53f
Andreas Plesch <andreasplesch@users.noreply.github.com>
2016-05-05 17:30:40 -0400

    Update aframe-faceset-component.js

--- components/mesh/aframe-faceset-component.js
+++ components/mesh/aframe-faceset-component.js
@@ -89,7 +89,7 @@ AFRAME.registerComponent('faceset', {
                           ( data.triangles.length !== g.faces.length ) ;
 
     if (geometryNeedsUpdate) {
-      g = updateGeometry(g, this.data, this.dmaps, facesNeedUpdate);
+      updateGeometry(g, this.data, this.dmaps, facesNeedUpdate);
     }
     
     if (translateNeedsUpdate) {
